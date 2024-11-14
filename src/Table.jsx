67ccfe9fe5007f0e6d8c70453c5847382bfa8c28
aroncales <aroncalesguillen@gmail.com>
2024-11-14 17:56:29 +0100

    Añadida funcionalidad a la tabla, ahora muestra una linea aleatoria por cada acceso

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,25 +1,40 @@
-import React, { useState } from 'react';
+import React, { useEffect , useState } from 'react';
 import busLinesData from './lines.json'
-
-const [busLine, setBusLine] = useState(null);
-
-
 
 export function TablaDinamica() {
 
+    console.log(busLinesData)
+
+    const[busLine, setBusLine] = useState(null);
+
+    const selectBusLine = () => {
+        const selectedLine = busLinesData.busLines[Math.floor(Math.random() * busLinesData.busLines.length)];
+        console.log("Línea seleccionada:", selectedLine);
+        setBusLine(selectedLine);
+    };
+    
+    useEffect(() => {
+        selectBusLine();
+    }, []);
+
+    if (!busLine) return <p>Cargando...</p>;
 
     return (
         <table>
-            <tr>
-                <td>Línea</td>
-                <td>Ruta</td>
-                <td>Operador</td>
-            </tr>
-            <tr>
-                <td>{numberLine}</td>
-                <td>{route}</td>
-                <td>{operator}</td>
-            </tr>
+            <thead>
+                <tr>
+                    <td>Línea</td>
+                    <td>Ruta</td>
+                    <td>Operador</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{busLine.numberLine}</td>
+                    <td>{busLine.route}</td>
+                    <td>{busLine.operator}</td>
+                </tr>
+            </tbody> 
         </table>
     )
 }
